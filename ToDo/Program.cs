68cfa7226ff04fa3c72b0cf87fb0a67d34dbf35d
Mariya Bukhryakova <mariya.bukhryakova@revature.com>
2022-01-03 12:06:20 -0500

    start react proj

---
+++ ToDo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo
+{
+         public class Program
+        {
+            // starting point of the application
+            public static void Main()
+            {
+                Console.WriteLine("Bridge.NET is up and running");
+            }
+        }
+    }
+}
